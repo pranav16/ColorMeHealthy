9529c04be936bafe12d2b4cfadec44354c92e4fd
pranav16 <GApplab@eaes-imac.uconnect.utah.edu>
2016-07-06 15:48:26 -0600

    fixed diary history

--- Assets/Scripts/DairyHistoryPage.cs
+++ Assets/Scripts/DairyHistoryPage.cs
@@ -11,10 +11,12 @@ public class DairyHistoryPage : MonoBehaviour {
 	public GameObject CLayerListOfDates;
 	public Text dairyEntry;
 	private Dictionary<string ,string> dateToMemo;
+	public List<Button> listOfDates;
 
 	// Use this for initialization
 	void Start () {
 		dateToMemo = new Dictionary<string, string> ();
+		listOfDates = new List<Button> ();	
 		readDairyJson ();
 		populateDateDropDowns ();
 	}
@@ -64,6 +66,9 @@ public class DairyHistoryPage : MonoBehaviour {
 	}
 	void populateListOfDates(string month,string year)
 	{
+		foreach (Button btn in listOfDates)
+			GameObject.Destroy (btn.gameObject);
+		listOfDates.Clear ();
 		List<string> dateEntries = new List<string> ();
 		foreach (string keys in dateToMemo.Keys) {
 			string[] dateSplit	= keys.Split ('_');
@@ -84,7 +89,7 @@ public class DairyHistoryPage : MonoBehaviour {
 			dateB.gameObject.SetActive (true);
 			dateB.gameObject.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (dateSelectorReference.gameObject.GetComponent<RectTransform>().anchoredPosition.x,postionY);
 			postionY -= dateSelectorReference.gameObject.GetComponent<RectTransform>().rect.height;
-
+			listOfDates.Add (dateB);
 		}
 
 	}
